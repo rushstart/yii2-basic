905bf8beefe396e533a2134d934f7aa0af65fe8e
petrov.do <petrov.od@avtozaim24.com>
2020-01-30 11:21:10 +0300

    dev

--- config/console.php
+++ config/console.php
@@ -4,8 +4,6 @@ return [
     'id' => 'app-console',
     'controllerNamespace' => 'app\commands',
     'controllerMap' => [
-        //Create migration in module extention -
-        //yii migrate/ --migrationPath=coderius/comments/migrations create_comments_table
         'migrate' => [
             'class' => 'yii\console\controllers\MigrateController',
             'migrationPath' => [
